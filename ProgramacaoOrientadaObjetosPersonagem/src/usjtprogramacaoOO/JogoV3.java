c11df63572c57b8255f82ad05e0ebfd9cb459be3
GustavoEvCardoso <107882018+GustavoEvCardoso@users.noreply.github.com>
2023-06-27 11:19:42 -0300

    Update JogoV3.java

--- ProgramacaoOrientadaObjetosPersonagem/src/usjtprogramacaoOO/JogoV3.java
+++ ProgramacaoOrientadaObjetosPersonagem/src/usjtprogramacaoOO/JogoV3.java
@@ -13,31 +13,18 @@ public class JogoV3 {
 			switch(random.nextInt(3)) { //vai sortear um numero ate 2, cada numero sorteado ele vai fazer algo que esta
 										//nos cases
 			case 0:
-				cacador.cacar();
-				lenhador.dormir();
-				lenhador.cacar();
+				pescador.cacar();
 				break;	
 				
 			case 1:
-				
-				cacador.dormir();
-				lenhador.dormir();
 				pescador.dormir();
 				break;
 			
 			case 2:
-				
-				cacador.comer();
-				lenhador.dormir();
 				pescador.comer();
 				break;
 				
 			}
-			
-			System.out.println(cacador.informacoes());
-			System.out.println("--------------------/n");
-			System.out.println(lenhador.informacoes());
-			System.out.println("--------------------/n");
 			System.out.println(pescador.informacoes());
 			Thread.sleep(2000);// serve para nao ir muito rapido na hora de exibir o system.out no console
 		}
