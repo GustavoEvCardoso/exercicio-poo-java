017199181a9ee0c4be1e52cb1c98de99ae75014f
GustavoEvCardoso <107882018+GustavoEvCardoso@users.noreply.github.com>
2023-06-27 11:24:38 -0300

    Update JogoV3.java

--- ProgramacaoOrientadaObjetosPersonagem/src/usjtprogramacaoOO/JogoV3.java
+++ ProgramacaoOrientadaObjetosPersonagem/src/usjtprogramacaoOO/JogoV3.java
@@ -6,26 +6,26 @@ public class JogoV3 {
 				
 	public static void main(String[] args) throws InterruptedException { //joga para outro quando der problema
 		Random random = new Random();
-		Personagem pescador = new Personagem ("Pedro",10,5,3);
+		Personagem cacador = new Personagem ("Pedro",10,5,3);
 
 		
 		while(true) {
 			switch(random.nextInt(3)) { //vai sortear um numero ate 2, cada numero sorteado ele vai fazer algo que esta
 										//nos cases
 			case 0:
-				pescador.cacar();
+				cacador.cacar();
 				break;	
 				
 			case 1:
-				pescador.dormir();
+				cacador.dormir();
 				break;
 			
 			case 2:
-				pescador.comer();
+				cacador.comer();
 				break;
 				
 			}
-			System.out.println(pescador.informacoes());
+			System.out.println(cacador.informacoes());
 			Thread.sleep(2000);// serve para nao ir muito rapido na hora de exibir o system.out no console
 		}
 	}
